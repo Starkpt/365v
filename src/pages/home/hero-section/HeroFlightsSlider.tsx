5d794d672fdf75a2b3642713d72d3da72b4ed240
Frederico Ramos <7763174+Starkpt@users.noreply.github.com>
2023-04-10 21:59:26 +0100

    smooth hover interaction on flight cards

--- src/pages/home/hero-section/HeroFlightsSlider.tsx
+++ src/pages/home/hero-section/HeroFlightsSlider.tsx
@@ -12,29 +12,47 @@ import "./heroFlightsSliderStyles.css";
 import MexicoCardImage from "@/assets/Viagens/América/América Latina/mexico/mexico.jpg";
 import MarrocosCardImage from "@/assets/Viagens/Árfrica/Marrocos/marrocos.jpg";
 import UzebequistanCardImage from "@/assets/Viagens/Ásia/Ásia Central/Uzbequistão.jpg";
+import { Box } from "@mui/system";
 
 const travels = [
-  { travelName: "México", rnavtRef: "RNAVT 1234", imgSrc: MexicoCardImage },
-  { travelName: "Marrocos", rnavtRef: "RNAVT 1234", imgSrc: MarrocosCardImage },
-  { travelName: "Uzebequistan", rnavtRef: "RNAVT 1234", imgSrc: UzebequistanCardImage },
-  { travelName: "México", rnavtRef: "RNAVT 1234", imgSrc: MexicoCardImage },
-  { travelName: "Marrocos", rnavtRef: "RNAVT 1234", imgSrc: MarrocosCardImage },
-  { travelName: "Uzebequistan", rnavtRef: "RNAVT 1234", imgSrc: UzebequistanCardImage },
-  { travelName: "México", rnavtRef: "RNAVT 1234", imgSrc: MexicoCardImage },
-  { travelName: "Marrocos", rnavtRef: "RNAVT 1234", imgSrc: MarrocosCardImage },
-  { travelName: "Uzebequistan", rnavtRef: "RNAVT 1234", imgSrc: UzebequistanCardImage },
+  { travelName: "México", rnavtRef: "RNAVT 1234", imgSrc: MexicoCardImage, price: 410 },
+  { travelName: "Marrocos", rnavtRef: "RNAVT 1234", imgSrc: MarrocosCardImage, price: 4123 },
+  { travelName: "Uzebequistan", rnavtRef: "RNAVT 1234", imgSrc: UzebequistanCardImage, price: 423 },
+  { travelName: "México", rnavtRef: "RNAVT 1234", imgSrc: MexicoCardImage, price: 2300 },
+  { travelName: "Marrocos", rnavtRef: "RNAVT 1234", imgSrc: MarrocosCardImage, price: 1232 },
+  { travelName: "Uzebequistan", rnavtRef: "RNAVT 1234", imgSrc: UzebequistanCardImage, price: 123 },
+  { travelName: "México", rnavtRef: "RNAVT 1234", imgSrc: MexicoCardImage, price: 311 },
+  { travelName: "Marrocos", rnavtRef: "RNAVT 1234", imgSrc: MarrocosCardImage, price: 598 },
+  { travelName: "Uzebequistan", rnavtRef: "RNAVT 1234", imgSrc: UzebequistanCardImage, price: 413 },
 ];
 
 const cardContentStyles = {
   width: "100%",
   background: "#5daeff52",
+  // @ts-ignore
+  background: "linear-gradient(180deg, rgba(9,43,95,0) 0%, rgba(9,43,95,1) 100%)",
   boxSizing: "border-box",
   height: "80px",
   display: "flex",
   alignItems: "center",
   padding: "0 !important",
-  transition: "background ease-in-out .2s",
-  ":hover": { background: "#2961c980" },
+  transition: "height ease-in-out .6s",
+
+  ".price-tag": {
+    position: "absolute",
+    translate: "0 70px",
+    transition: "translate ease-in-out .5s",
+  },
+
+  ":hover": {
+    background:
+      "linear-gradient(180deg, rgba(0,0,0,0) 0%,  rgba(9,43,95,0.15) 15%, rgba(9,43,95,1) 100%)",
+    height: "100px",
+    ".price-tag": {
+      position: "absolute",
+      translate: "0 50px",
+    },
+  },
 };
 
 const cardActionAreaStyles = {
@@ -101,13 +119,23 @@ function HeroFlightsSlider() {
         },
       }}
     >
-      {travels.map(({ travelName, rnavtRef, imgSrc }, id) => {
+      {travels.map(({ travelName, rnavtRef, imgSrc, price }, id) => {
         return (
           <SwiperSlide key={id} virtualIndex={id} style={{ width: "150px", height: "250px" }}>
             <Card raised sx={{ ...cardActionAreaStyles, backgroundImage: `url("${imgSrc}")` }}>
               {/* <CardMedia component="img" height="100%" image={MexicoCardImage} alt="green iguana" /> */}
               <CardContent sx={cardContentStyles}>
-                <CardActionArea sx={{ height: "100%" }}>
+                <CardActionArea
+                  sx={{
+                    height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignContent: "left",
+                    alignItems: "flex-start",
+                    padding: "10px",
+                    justifyContent: "start",
+                  }}
+                >
                   <Typography variant="subtitle1" sx={{ fontWeight: 500 }} color={"ivory"}>
                     {travelName}
                   </Typography>
@@ -117,6 +145,16 @@ function HeroFlightsSlider() {
                     sx={{ fontSize: "11px", fontWeight: 300 }}
                   >
                     {rnavtRef}
+                  </Typography>
+                  <Typography
+                    className="price-tag"
+                    variant="button"
+                    color="ivory"
+                    bgcolor={"green"}
+                    borderRadius={1}
+                    sx={{ fontSize: "11px", fontWeight: 500, padding: "2px 6px" }}
+                  >
+                    {price}€
                   </Typography>
                 </CardActionArea>
               </CardContent>

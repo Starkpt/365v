3172f4d042e561ff4e2a8857bea5e0f2eae20d6b
Frederico Ramos <7763174+Starkpt@users.noreply.github.com>
2023-04-12 19:47:15 +0100

    Hero section code splitting

--- src/pages/home/hero-section/HeroFlightsSlider.tsx
+++ src/pages/home/hero-section/HeroFlightsSlider.tsx
@@ -1,4 +1,4 @@
-import { Card, CardActionArea } from "@mui/material";
+import { Card, CardActionArea, Divider } from "@mui/material";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import { Navigation, Pagination } from "swiper";
@@ -12,7 +12,6 @@ import "./heroFlightsSliderStyles.css";
 import MexicoCardImage from "@/assets/Viagens/América/América Latina/mexico/mexico.jpg";
 import MarrocosCardImage from "@/assets/Viagens/Árfrica/Marrocos/marrocos.jpg";
 import UzebequistanCardImage from "@/assets/Viagens/Ásia/Ásia Central/Uzbequistão.jpg";
-import { Box } from "@mui/system";
 
 const travels = [
   { travelName: "México", rnavtRef: "RNAVT 1234", imgSrc: MexicoCardImage, price: 410 },
@@ -69,103 +68,109 @@ const cardActionAreaStyles = {
 
 function HeroFlightsSlider() {
   return (
-    <Swiper
-      // loop={true}
-      // spaceBetween={15}
-      // slidesPerView={4}
-      // slidesPerView={"auto"}
-      // centeredSlides={true}
-      // watchSlidesProgress={true}
-      className="mySwiper"
-      modules={[Pagination, Navigation]}
-      pagination={{ clickable: true }}
-      breakpoints={{
-        200: {
-          slidesPerGroup: 1,
-          slidesPerView: 1,
-          spaceBetween: 30,
-        },
-        576: {
-          slidesPerGroup: 1,
-          slidesPerView: 1,
-          spaceBetween: 30,
-        },
-        640: {
-          slidesPerGroup: 1,
-          slidesPerView: 1,
-          spaceBetween: 30,
-        },
-        768: {
-          slidesPerGroup: 2,
-          slidesPerView: 2,
-          spaceBetween: 30,
-        },
-        992: {
-          slidesPerGroup: 2,
-          slidesPerView: 2,
-          spaceBetween: 30,
-        },
-        1024: {
-          slidesPerGroup: 3,
-          slidesPerView: 3,
-          spaceBetween: 30,
-        },
-        1400: {
-          slidesPerGroup: 4,
-          slidesPerView: 4,
-          spaceBetween: 30,
-        },
-        1536: {
-          slidesPerGroup: 5,
-          slidesPerView: 5,
-          spaceBetween: 30,
-        },
-      }}
-    >
-      {travels.map(({ travelName, rnavtRef, imgSrc, price }, id) => {
-        return (
-          <SwiperSlide key={id} virtualIndex={id} style={{ width: "150px", height: "250px" }}>
-            <Card raised sx={{ ...cardActionAreaStyles, backgroundImage: `url("${imgSrc}")` }}>
-              {/* <CardMedia component="img" height="100%" image={MexicoCardImage} alt="green iguana" /> */}
-              <CardContent sx={cardContentStyles}>
-                <CardActionArea
-                  sx={{
-                    height: "100%",
-                    display: "flex",
-                    flexDirection: "column",
-                    alignContent: "left",
-                    alignItems: "flex-start",
-                    padding: "10px",
-                    justifyContent: "start",
-                  }}
-                >
-                  <Typography variant="subtitle1" sx={{ fontWeight: 500 }} color={"ivory"}>
-                    {travelName}
-                  </Typography>
-                  <Typography
-                    variant="caption"
-                    color="ivory"
-                    sx={{ fontSize: "11px", fontWeight: 300 }}
+    <>
+      <Typography variant="h5" sx={{ textAlign: "left", fontWeight: 500 }} color={"primary.main"}>
+        Promotions
+      </Typography>
+      <Divider sx={{ marginBottom: "24px" }} />
+      <Swiper
+        // loop={true}
+        // spaceBetween={15}
+        // slidesPerView={4}
+        // slidesPerView={"auto"}
+        // centeredSlides={true}
+        // watchSlidesProgress={true}
+        className="mySwiper"
+        modules={[Pagination, Navigation]}
+        pagination={{ clickable: true }}
+        breakpoints={{
+          200: {
+            slidesPerGroup: 1,
+            slidesPerView: 1,
+            spaceBetween: 30,
+          },
+          576: {
+            slidesPerGroup: 1,
+            slidesPerView: 1,
+            spaceBetween: 30,
+          },
+          640: {
+            slidesPerGroup: 1,
+            slidesPerView: 1,
+            spaceBetween: 30,
+          },
+          768: {
+            slidesPerGroup: 2,
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+          992: {
+            slidesPerGroup: 2,
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+          1024: {
+            slidesPerGroup: 3,
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+          1400: {
+            slidesPerGroup: 4,
+            slidesPerView: 4,
+            spaceBetween: 30,
+          },
+          1536: {
+            slidesPerGroup: 5,
+            slidesPerView: 5,
+            spaceBetween: 30,
+          },
+        }}
+      >
+        {travels.map(({ travelName, rnavtRef, imgSrc, price }, id) => {
+          return (
+            <SwiperSlide key={id} virtualIndex={id} style={{ width: "150px", height: "250px" }}>
+              <Card raised sx={{ ...cardActionAreaStyles, backgroundImage: `url("${imgSrc}")` }}>
+                {/* <CardMedia component="img" height="100%" image={MexicoCardImage} alt="green iguana" /> */}
+                <CardContent sx={cardContentStyles}>
+                  <CardActionArea
+                    sx={{
+                      height: "100%",
+                      display: "flex",
+                      flexDirection: "column",
+                      alignContent: "left",
+                      alignItems: "flex-start",
+                      padding: "10px",
+                      justifyContent: "start",
+                    }}
                   >
-                    {rnavtRef}
-                  </Typography>
-                  <Typography
-                    className="price-tag"
-                    variant="button"
-                    color="ivory"
-                    bgcolor={"green"}
-                    borderRadius={1}
-                    sx={{ fontSize: "11px", fontWeight: 500, padding: "2px 6px" }}
-                  >
-                    {price}€
-                  </Typography>
-                </CardActionArea>
-              </CardContent>
-            </Card>
-          </SwiperSlide>
-        );
-      })}
-    </Swiper>
+                    <Typography variant="subtitle1" sx={{ fontWeight: 500 }} color={"ivory"}>
+                      {travelName}
+                    </Typography>
+                    <Typography
+                      variant="caption"
+                      color="ivory"
+                      sx={{ fontSize: "11px", fontWeight: 300 }}
+                    >
+                      {rnavtRef}
+                    </Typography>
+                    <Typography
+                      className="price-tag"
+                      variant="button"
+                      color="ivory"
+                      bgcolor={"green"}
+                      borderRadius={1}
+                      sx={{ fontSize: "11px", fontWeight: 500, padding: "2px 6px" }}
+                    >
+                      {price}€
+                    </Typography>
+                  </CardActionArea>
+                </CardContent>
+              </Card>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </>
   );
 }
 
---
+++ src/pages/home/hero-section/HeroSearchNGo.tsx
@@ -0,0 +1,177 @@
+import {
+  Button,
+  Divider,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  Typography,
+} from "@mui/material";
+import { Box, Stack } from "@mui/system";
+import { DateTimeField, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs, { Dayjs } from "dayjs";
+import { useState } from "react";
+
+const dateTimeFieldStyles = {
+  flex: 1,
+  label: {
+    // color: "#041DB8",
+    fontFamily: "sans-serif",
+    fontSize: ".90rem",
+  },
+  "input.MuiInputBase-input.MuiOutlinedInput-input": {
+    height: "32px",
+    backgroundColor: "#F8F8F8",
+    padding: "0px 14px",
+    fontFamily: "sans-serif",
+    fontSize: "0.80rem",
+    fontWeight: 600,
+    // color: "#041DB8",
+  },
+  fieldset: {
+    // border: "none",
+  },
+};
+
+const resizableLabelStyles = {
+  flex: 1,
+  fontFamily: "sans-serif",
+  fontSize: ".90rem",
+  height: "100%",
+  lineHeight: 1,
+  top: "0",
+};
+
+const locationStyles = {
+  width: "100%",
+  height: "32px",
+  textAlign: "left",
+  fontFamily: "sans-serif",
+  fontSize: "0.80rem",
+  fontWeight: 600,
+};
+
+function HeroSearchNGo() {
+  const [departureDate, setDepartureDate] = useState<Dayjs | null>(dayjs("2022-04-17T15:30"));
+  const [returnDate, setReturnDate] = useState<Dayjs | null>(dayjs("2022-04-17T15:30"));
+  const [locationFrom, setLocationFrom] = useState("");
+  const [locationTo, setLocationTo] = useState("");
+  const [passengersNo, setPassengersNo] = useState("");
+
+  return (
+    <>
+      <Typography variant="h5" sx={{ textAlign: "left", fontWeight: 500 }} color={"primary.main"}>
+        Search & Go
+      </Typography>
+      <Divider sx={{ marginBottom: "24px" }} />
+
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: { xs: "column", sm: "column", md: "column", lg: "row", xl: "row" },
+            gap: { xs: "16px", sm: "16px", md: "16px", lg: "12px", xl: "12px" },
+          }}
+        >
+          <Stack
+            direction={{ xs: "column", sm: "column", md: "row", lg: "row", xl: "row" }}
+            width={{ xs: "100%", sm: "100%", md: "100%", lg: "50%", xl: "33%" }}
+            gap={{ xs: "16px", sm: "16px", md: "16px", lg: "12px", xl: "12px" }}
+            useFlexGap
+            flexWrap="wrap"
+          >
+            <DateTimeField
+              sx={dateTimeFieldStyles}
+              label="Departure"
+              value={departureDate}
+              onChange={(newDeparture) => setDepartureDate(newDeparture)}
+              format="DD/MM/YYYY"
+            />
+            <DateTimeField
+              sx={dateTimeFieldStyles}
+              label="Return"
+              value={returnDate}
+              onChange={(newReturn) => setReturnDate(newReturn)}
+              format="DD/MM/YYYY"
+            />
+          </Stack>
+
+          <Stack
+            direction={{ xs: "column", sm: "column", md: "column", lg: "row", xl: "row" }}
+            width={{ xs: "100%", sm: "100%", md: "100%", lg: "50%", xl: "33%" }}
+            gap={{ xs: "16px", sm: "16px", md: "16px", lg: "12px", xl: "12px" }}
+            useFlexGap
+            flexWrap="wrap"
+          >
+            <FormControl sx={{ flex: 1 }} size="small">
+              <InputLabel sx={resizableLabelStyles}>From</InputLabel>
+              <Select
+                value={locationFrom}
+                label="From"
+                sx={locationStyles}
+                onChange={(e: SelectChangeEvent) => setLocationFrom(e.target.value)}
+              >
+                <MenuItem value="">-</MenuItem>
+                <MenuItem value="madrid">Madrid</MenuItem>
+                <MenuItem value="lisbon">Lisboa</MenuItem>
+                <MenuItem value="london">Londres</MenuItem>
+                <MenuItem value="paris">Paris</MenuItem>
+                <MenuItem value="porto">Porto</MenuItem>
+              </Select>
+            </FormControl>
+
+            <FormControl sx={{ flex: 1 }} size="small">
+              <InputLabel sx={resizableLabelStyles}>To</InputLabel>
+              <Select
+                value={locationTo}
+                label="To"
+                sx={locationStyles}
+                onChange={(e: SelectChangeEvent) => setLocationTo(e.target.value)}
+              >
+                <MenuItem value="">-</MenuItem>
+                <MenuItem value="madrid">Madrid</MenuItem>
+                <MenuItem value="lisbon">Lisboa</MenuItem>
+                <MenuItem value="london">Londres</MenuItem>
+                <MenuItem value="paris">Paris</MenuItem>
+                <MenuItem value="porto">Porto</MenuItem>
+              </Select>
+            </FormControl>
+          </Stack>
+
+          <Stack
+            direction={{ xs: "column", sm: "row", md: "row", lg: "row", xl: "row" }}
+            width={{ xs: "100%", sm: "100%", md: "100%", lg: "50%", xl: "33%" }}
+            gap={{ xs: "16px", sm: "16px", md: "16px", lg: "12px", xl: "12px" }}
+            useFlexGap
+            flexWrap="wrap"
+          >
+            <FormControl sx={{ flex: 2 }} size="small">
+              <InputLabel sx={resizableLabelStyles}>Passengers</InputLabel>
+              <Select
+                value={passengersNo}
+                label="Passengers"
+                sx={locationStyles}
+                onChange={(e: SelectChangeEvent) => setPassengersNo(e.target.value)}
+              >
+                <MenuItem value="">-</MenuItem>
+                <MenuItem value="1adults">1 Adults, Economy</MenuItem>
+                <MenuItem value="2adults">2 Adults, Economy</MenuItem>
+                <MenuItem value="1adults-1child">1 Adults + 1 Child, Economy</MenuItem>
+                <MenuItem value="1adults-1child">1 Adults + 2 Child, Economy</MenuItem>
+                <MenuItem value="etc">etc..</MenuItem>
+              </Select>
+            </FormControl>
+
+            <Button sx={{ height: "32px" }} variant="contained">
+              Search
+            </Button>
+          </Stack>
+        </Box>
+      </LocalizationProvider>
+    </>
+  );
+}
+
+export default HeroSearchNGo;

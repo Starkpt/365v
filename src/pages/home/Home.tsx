d74a55dec386eb6f61f7c246dda7323c9398de83
Frederico Ramos <7763174+Starkpt@users.noreply.github.com>
2023-04-23 15:37:33 +0100

    Fly with us button reorganizing

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,43 @@
 import { Box } from "@mui/system";
 import HeroSection from "./hero-section/HeroSection";
+import { Button, Typography } from "@mui/material";
+import ImgPlane from "@/assets/plane.png";
 
+const flyWithUsButtonStyles = {
+  borderRadius: "0",
+  height: "90px",
+  width: "100%",
+  backgroundColor: "white",
+  boxShadow:
+    "0px -5px 5px -3px rgba(0,0,0,0.2), 0px -8px 10px -1px rgba(0,0,0,0.14), 0px -3px 4px -2px rgba(0,0,0,0.12)",
+
+  ":hover": {
+    boxShadow:
+      "0px -5px 5px -3px rgba(0,0,0,0.2), 0px -8px 10px -1px rgba(0,0,0,0.14), 0px -3px 4px -2px rgba(0,0,0,0.12) !important",
+    background: "#b6dcf8",
+    //@ts-expect-error
+    background: " linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(182,220,248,1) 100%)",
+  },
+};
+
+const flyWithUsTypographyStyles = {
+  marginLeft: "4px",
+  textTransform: "initial",
+  textAlign: "left",
+  fontWeight: 500,
+};
 const HomePage = () => {
   return (
     <Box>
       <HeroSection />
+
+      <Button variant="contained" href="#text-buttons" sx={flyWithUsButtonStyles}>
+        <img src={ImgPlane} alt="Fly with us" />
+        <Typography variant="body1" color={"primary.main"} sx={flyWithUsTypographyStyles}>
+          Fly with us.
+        </Typography>
+      </Button>
+
       
       {/* <Unstable_Grid2 container>
         <Unstable_Grid2 xs={8} sm={8} md={8} lg={8} xl={8}>
--- src/pages/home/hero-section/HeroSection.tsx
+++ src/pages/home/hero-section/HeroSection.tsx
@@ -2,7 +2,6 @@ import HeroSlider from "@/components/hero-slider/HeroSlider";
 import { Box, Card, Button, Typography } from "@mui/material";
 import HeroFlightsSlider from "./HeroFlightsSlider";
 import HeroSearchNGo from "./HeroSearchNGo";
-import ImgPlane from "@/assets/plane.png";
 
 export const heroSectionResponsiveStyles = {
   height: {
@@ -38,6 +37,7 @@ const cardStyles = {
   zIndex: 1,
 };
 
+
 function HeroSection() {
   return (
     <Box sx={heroSectionStyles}>
@@ -46,43 +46,6 @@ function HeroSection() {
         <HeroFlightsSlider />
         <HeroSearchNGo />
       </Card>
-      <Button
-        variant="contained"
-        href="#text-buttons"
-        sx={{
-          position: "absolute",
-          bottom: "-90px",
-          zIndex: 1,
-          borderRadius: "0",
-          height: "90px",
-          width: "100%",
-          backgroundColor: "white",
-          boxShadow:
-            "0px -5px 5px -3px rgba(0,0,0,0.2), 0px -8px 10px -1px rgba(0,0,0,0.14), 0px -3px 4px -2px rgba(0,0,0,0.12)",
-
-          ":hover": {
-            boxShadow:
-              "0px -5px 5px -3px rgba(0,0,0,0.2), 0px -8px 10px -1px rgba(0,0,0,0.14), 0px -3px 4px -2px rgba(0,0,0,0.12) !important",
-            background: "#b6dcf8",
-            //@ts-expect-error
-            background: " linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(182,220,248,1) 100%)",
-          },
-        }}
-      >
-        <img src={ImgPlane} alt="Fly with us" />
-        <Typography
-          variant="body1"
-          sx={{
-            marginLeft: "4px",
-            textTransform: "initial",
-            textAlign: "left",
-            fontWeight: 500,
-          }}
-          color={"primary.main"}
-        >
-          Fly with us.
-        </Typography>
-      </Button>
     </Box>
   );
 }

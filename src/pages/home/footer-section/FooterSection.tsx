8d0ed0d121777043f229f2d3cfd9767a4f3e4ca0
Frederico Ramos <7763174+Starkpt@users.noreply.github.com>
2023-09-18 01:17:03 +0100

    Start footer section

---
+++ src/pages/home/footer-section/FooterSection.tsx
@@ -0,0 +1,86 @@
+// Libraries
+import { Button, Box, Typography } from "@mui/material";
+import { Stack } from "@mui/system";
+
+// Resources
+import Logo from "@/assets/logo-footer.svg";
+import SocialsFacebook from "@/assets/socials-facebook.png";
+
+export const footerResponsiveStyles = {
+  height: {
+    // xs: "50vh",
+    // sm: "50vh",
+    // md: "50vh",
+    // lg: "60vh",
+    // xl: "60vh",
+  },
+  minHeight: {
+    // xs: "500px",
+    // sm: "550px",
+    // md: "550px",
+    // lg: "600px",
+    // xl: "600px",
+  },
+};
+
+const footerSectionStyles = {
+  ...footerResponsiveStyles,
+  width: "100%",
+  padding: "70px 0",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  position: "relative",
+  backgroundColor: "#092B5F",
+  color: "white",
+};
+
+const FooterSection = () => {
+  return (
+    <Box sx={footerSectionStyles}>
+      <Stack
+        sx={{ justifyContent: "center" }}
+        direction={{ xs: "column", sm: "column", md: "row", lg: "row", xl: "row" }}
+        width={{ xs: "100%", sm: "100%", md: "100%", lg: "50%", xl: "33%" }}
+        gap={{ xs: "16px", sm: "16px", md: "16px", lg: "75px", xl: "75px" }}
+        useFlexGap
+      >
+        <img src={Logo} alt="365 Viagens" />
+        <Box
+          sx={{
+            textAlign: "left",
+            "& > *": {
+              marginBottom: "8px !important",
+            },
+            a: {
+              color: "white",
+              textDecoration: "none",
+              display: "block",
+              ":hover": {
+                fontWeight: 500,
+              },
+
+              ":first-of-type": {
+                marginTop: "24px",
+              },
+            },
+          }}
+        >
+          <Typography variant="body2" fontSize={"16px"} fontWeight={500}>
+            365 Agência de Viagens
+          </Typography>
+          <Typography variant="body2">+351 965 705 060</Typography>
+          <Typography variant="body2">reservas@365viagens.com</Typography>
+          <Typography variant="body2" component={"a"} href="#">
+            Livro de Reclamações
+          </Typography>
+          <a href="#">
+            <img src={SocialsFacebook} alt="365 Viagens" />
+          </a>
+        </Box>
+      </Stack>
+    </Box>
+  );
+};
+
+export default FooterSection;

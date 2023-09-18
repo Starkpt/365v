eb7a1aacc97fc20bce3f18735fba200fc7d84209
Frederico Ramos <7763174+Starkpt@users.noreply.github.com>
2023-09-19 00:46:51 +0100

    Add footer section "made by"

--- src/pages/home/footer-section/FooterSection.tsx
+++ src/pages/home/footer-section/FooterSection.tsx
@@ -35,51 +35,66 @@ const footerSectionStyles = {
   color: "white",
 };
 
+const footerBySectionStyles = {
+  width: "100%",
+  padding: "4px 0",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  position: "relative",
+  backgroundColor: "#133365",
+  color: "white",
+};
+
 const FooterSection = () => {
   return (
-    <Box sx={footerSectionStyles}>
-      <Stack
-        sx={{ justifyContent: "center" }}
-        direction={{ xs: "column", sm: "column", md: "row", lg: "row", xl: "row" }}
-        width={{ xs: "100%", sm: "100%", md: "100%", lg: "50%", xl: "33%" }}
-        gap={{ xs: "16px", sm: "16px", md: "16px", lg: "75px", xl: "75px" }}
-        useFlexGap
-      >
-        <img src={Logo} alt="365 Viagens" />
-        <Box
-          sx={{
-            textAlign: "left",
-            "& > *": {
-              marginBottom: "8px !important",
-            },
-            a: {
-              color: "white",
-              textDecoration: "none",
-              display: "block",
-              ":hover": {
-                fontWeight: 500,
-              },
-
-              ":first-of-type": {
-                marginTop: "24px",
-              },
-            },
-          }}
+    <>
+      <Box sx={footerSectionStyles}>
+        <Stack
+          sx={{ justifyContent: "center" }}
+          direction={{ xs: "column", sm: "column", md: "row", lg: "row", xl: "row" }}
+          width={{ xs: "100%", sm: "100%", md: "100%", lg: "50%", xl: "33%" }}
+          gap={{ xs: "16px", sm: "16px", md: "16px", lg: "75px", xl: "75px" }}
+          useFlexGap
         >
-          <Typography variant="body2" fontSize={"16px"} fontWeight={500}>
-            365 Agência de Viagens
-          </Typography>
-          <Typography variant="body2">+351 965 705 060</Typography>
-          <Typography variant="body2">reservas@365viagens.com</Typography>
-          <Typography variant="body2" component={"a"} href="#">
-            Livro de Reclamações
-          </Typography>
-          <a href="#">
-            <img src={SocialsFacebook} alt="365 Viagens" />
-          </a>
-        </Box>
-      </Stack>
-    </Box>
+          <img src={Logo} alt="365 Viagens" />
+          <Box
+            sx={{
+              textAlign: "left",
+              "& > *": {
+                marginBottom: "8px !important",
+              },
+              a: {
+                color: "white",
+                textDecoration: "none",
+                display: "block",
+                ":hover": {
+                  fontWeight: 500,
+                },
+
+                ":first-of-type": {
+                  marginTop: "24px",
+                },
+              },
+            }}
+          >
+            <Typography variant="body2">+351 965 705 060</Typography>
+            <Typography variant="body2">reservas@365viagens.com</Typography>
+            <Typography variant="body2" component={"a"} href="#">
+              Livro de Reclamações
+            </Typography>
+            <a href="#">
+              <img src={SocialsFacebook} alt="365 Viagens" />
+            </a>
+          </Box>
+        </Stack>
+      </Box>
+      <Box sx={footerBySectionStyles}>
+        <Typography fontSize={"12px"} variant="body2">
+          © 365 Agência de Viagens | by Branches
+        </Typography>
+      </Box>
+    </>
   );
 };
 
